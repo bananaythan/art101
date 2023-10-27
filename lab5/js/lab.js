1b60ac23a67f3ab55644988c14f540053072d963
Nathan Kim <namakim@ucsc.edu>
2023-10-26 21:38:15 -0700

    lab 5 complete

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,6 +1,21 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - js file for lab 5
+// Author: Nathan Kim
+// Date: 26 October 2023
+
+// Variables
+make = "Hyundai";
+model = "Sonata";
+color = "White";
+year = 2011;
+
+age = 2023 - year;
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
 
 // Constants
 
