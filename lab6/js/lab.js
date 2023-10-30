ef5c26a5211e048357d2296ac9b0c86e5c104c76
Nathan Kim <namakim@ucsc.edu>
2023-10-30 02:16:53 -0700

    lab 6 completion

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,6 +1,23 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - js file for lab 6
+// Author: Nathan Kim
+// Date: 29 October 2023
+
+const myTransport = ["car", "bus"]
+var myMainRide = {
+  make: "Hyundai",
+  model: "Sonata",
+  color: "white",
+  year: 2011,
+  age: function () {
+      return 2023 - this.year;
+  }
+}
+
+document.writeln("Getting around Santa Cruz:" + myTransport + "<br>");
+document.writeln("My Main Ride: <pre>",
+  JSON.stringify(myMainRide, null, '\t'), "</pre>");
+
+
 
 // Constants
 
